4c4858c7ed8e0e78ec9a71602af5c9c31b50668c
Kristiyan <kriskata50@gmail.com>
2025-07-09 15:34:59 +0300

    Init.

---
+++ src/test/java/project/kristiyan/WebServer/WebServerApplicationTest.java
@@ -0,0 +1,11 @@
+package project.kristiyan.WebServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class WebServerApplicationTest {
+
+    @Test
+    void contextLoad() {}
+}
